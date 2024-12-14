788f748b576c8d98d29ed1809828974c076f3321
Jānis Rāzna <janis.razna@va.lv>
2024-12-14 22:38:26 +0200

    Chore: teksts

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#pirmais mājasdarbs
+# pirmais mājasdarbs
+# Uzd. samainīt versijas:
+# 1. terminālī: py -0 būs redzamas pieejamās versijas
+# 1.1. ar * esošā būs noklusējuma. Man ir -V:3.13*; -V:3.11; -V3.10
+# 1.2. izvēloties py -3.11 būs Python 3.11.9
+# 1.3. izvēloties py -3.10 būs Python 3.10.11
+# 2.VSCode: apakšējā labajā stūrī redzama versija;
+# 2.1. uz tās uzklikšķinot augšā pa vidu parādīsies Select Interpreter
+# 2.2. izvēlas attiecīgi versiju
+# 2.3. uzspiež augšējā labajā stūrī Run Python File
+# 2.4. redzu, ka var atkārtot līdzīgas darbības kā pirmajā punktā.
